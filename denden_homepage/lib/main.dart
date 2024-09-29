7e6936e868187b7d069e037e4d45a45eb6979b46
yuubinnkyoku <yuubinnkyoku0730@gmail.com>
2024-09-29 19:24:52 +0900

    Update main.dart

--- denden_homepage/lib/main.dart
+++ denden_homepage/lib/main.dart
@@ -10,60 +10,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '電子電脳技術研究会',
+      title: '部活名',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true, // Material Design 3を有効化
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: '電子電脳技術研究会ホームページ'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'ようこそ！',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // ここにアクションを追加
-              },
-              child: const Text('活動内容'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // ここにアクションを追加
-              },
-              child: const Text('作品紹介'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // ここにアクションを追加
-              },
-              child: const Text('SNS'),
-            ),
+        title: const Text('部活名ホームページ'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: '活動内容'),
+            Tab(text: '作品・功績紹介'),
+            Tab(text: 'SNS'),
           ],
         ),
       ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          ActivitiesTab(),
+          WorksTab(),
+          SNSTab(),
+        ],
+      ),
+    );
+  }
+}
+
+class ActivitiesTab extends StatelessWidget {
+  const ActivitiesTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('活動内容'),
+    );
+  }
+}
+
+class WorksTab extends StatelessWidget {
+  const WorksTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('作品・功績紹介'),
+    );
+  }
+}
+
+class SNSTab extends StatelessWidget {
+  const SNSTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('SNS'),
     );
   }
 }
